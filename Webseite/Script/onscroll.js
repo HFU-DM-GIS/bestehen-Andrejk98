8f67feafbc328307cd2b34c914715480b4841377
marcel2160 <marcel.hepting@hs-furtwangen.de>
2023-05-02 01:29:40 +0200

    ungewünschter Text unten nicht mehr zu sehen

--- Webseite/Script/onscroll.js
+++ Webseite/Script/onscroll.js
@@ -2,6 +2,8 @@ let hi = document.getElementById("hi");
 let bo = document.getElementById("bo");
 let bi = document.getElementById("bi");
 let fi = document.getElementById("fi");
+let div = document.getElementById("zwei");
+
 window.addEventListener('scroll',function()
 {
     var value = window.scrollY;
@@ -10,7 +12,24 @@ window.addEventListener('scroll',function()
     bo.style.top = -value * 0.15 + 'px';
     text.style.top = value * 1 + 'px';
     
+    if (value > 500) {
+        text.style.opacity = '0';
+        hi.style.opacity = '0';
+    } else {
+        text.style.opacity = '1';
+        hi.style.opacity = '1';  
+    }
+
+    
+
+    
+
+
+
 })
+
+
+
 
 
 
