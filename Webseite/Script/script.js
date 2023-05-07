93e153184426ccffcd26ea18ff87a68d452de555
GioddiTave <judith.dorothee.tavella@hs-furtwangen.de>
2023-05-07 19:01:08 +0200

    js-change File: script.js Added: collision function  & checkLose function (nicht sicher ob man die Bracuht)

--- Webseite/Script/script.js
+++ Webseite/Script/script.js
@@ -35,3 +35,17 @@ function update(time) {
   lastTime = time
   window.requestAnimationFrame(update)
 }
+
+function checkLose() {
+    const unicornRect = getUnicornRect()
+    return getFlowerRects().some(rect => isCollision(rect, unicornRect))
+  }
+  
+  function isCollision(rect1, rect2) {
+    return (
+      rect1.left < rect2.right &&
+      rect1.top < rect2.bottom &&
+      rect1.right > rect2.left &&
+      rect1.bottom > rect2.top
+    )
+  }
