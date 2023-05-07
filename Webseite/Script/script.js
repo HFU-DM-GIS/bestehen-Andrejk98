fee96d55ec7172f0d9c09e6bb44c469659e01064
GioddiTave <judith.dorothee.tavella@hs-furtwangen.de>
2023-05-07 18:49:35 +0200

    js-change File : script.js definition of const -world width/scale -speed scale increase (schneller werden beim Spiel) -startscreen Element -World Element -Score Element

--- Webseite/Script/script.js
+++ Webseite/Script/script.js
@@ -1,3 +1,11 @@
 import { updateGround, setupGround } from "./ground.js"
 import { updateUnicorn, setupUnicorn, getUnicornRect, setUnicornLose } from "./unicorn.js"
 import { updateFlower, setupFlower, getFlowerRects } from "./flower.js"
+
+const WORLD_WIDTH = 100
+const WORLD_HEIGHT = 30
+const SPEED_SCALE_INCREASE = 0.00001
+
+const worldElem = document.querySelector("[data-world]")
+const scoreElem = document.querySelector("[data-score]")
+const startScreenElem = document.querySelector("[data-start-screen]")
