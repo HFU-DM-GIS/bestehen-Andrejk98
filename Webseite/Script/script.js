a81a4f7a464dbc8ed7d214d1bc97a37dcc5ffc78
GioddiTave <judith.dorothee.tavella@hs-furtwangen.de>
2023-05-07 18:45:32 +0200

    Js-Change File: script Verlinkung zu: -ground.js -unicorn.js -flower.js für Import.

---
+++ Webseite/Script/script.js
@@ -0,0 +1,3 @@
+import { updateGround, setupGround } from "./ground.js"
+import { updateUnicorn, setupUnicorn, getUnicornRect, setUnicornLose } from "./unicorn.js"
+import { updateFlower, setupFlower, getFlowerRects } from "./flower.js"
