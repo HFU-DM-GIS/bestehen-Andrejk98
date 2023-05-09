f1490d06a9b0af75d8f059eebada2148495e46bc
GioddiTave <judith.dorothee.tavella@hs-furtwangen.de>
2023-05-09 17:59:28 +0200

    js-change file:Unicorn.js Added: new Js file Unicorn

---
+++ Webseite/Script/unicorn.js
@@ -0,0 +1,72 @@
+import {
+    incrementCustomProperty,
+    setCustomProperty,
+    getCustomProperty,
+  } from "./updateCustomProperty.js"
+  
+  const unicornElem = document.querySelector("[data-unicorn]")
+  const JUMP_SPEED = 0.45
+  const GRAVITY = 0.0015
+  const UNICORN_FRAME_COUNT = 2 
+  const FRAME_TIME = 100
+  
+  let isJumping
+  let unicornFrame
+  let currentFrameTime
+  let yVelocity
+  export function setupUnicorn() {
+    isJumping = false
+    unicornFrame = 0
+    currentFrameTime = 0
+    yVelocity = 0
+    setCustomProperty(unicornElem, "--bottom", 0)
+    document.removeEventListener("keydown", onJump)
+    document.addEventListener("keydown", onJump)
+  }
+  
+  export function updateUnicorn(delta, speedScale) {
+    handleRun(delta, speedScale)
+    handleJump(delta)
+  }
+  
+  export function getUnicornRect() {
+    return unicornElem.getBoundingClientRect()
+  }
+  
+  export function setUnicornLose() {
+    unicornElem.src = "./Unicorn.png"
+  }
+  
+  function handleRun(delta, speedScale) {
+    if (isJumping) {
+      unicornElem.src = `./Unicorn.png`
+      return
+    }
+  
+    /*if (currentFrameTime >= FRAME_TIME) {
+      dinoFrame = (dinoFrame + 1) % DINO_FRAME_COUNT 
+      dinoElem.src = `./Unicorn${dinoFrame}.png`
+      currentFrameTime -= FRAME_TIME
+    }*/
+    currentFrameTime += delta * speedScale
+  }
+  
+  function handleJump(delta) {
+    if (!isJumping) return
+  
+    incrementCustomProperty(unicornElem, "--bottom", yVelocity * delta)
+  
+    if (getCustomProperty(unicornElem, "--bottom") <= 0) {
+      setCustomProperty(unicornElem, "--bottom", 0)
+      isJumping = false
+    }
+  
+    yVelocity -= GRAVITY * delta
+  }
+  
+  function onJump(e) {
+    if (e.code !== "Space" || isJumping) return
+  
+    yVelocity = JUMP_SPEED
+    isJumping = true
+  }
