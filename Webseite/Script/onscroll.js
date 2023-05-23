5b8eab81cba1c1ba3a58967fd761d35ff5d92dc1
marcel2160 <marcel.hepting@hs-furtwangen.de>
2023-05-24 01:07:31 +0200

    Scroll-Trigger hinzugefügt

--- Webseite/Script/onscroll.js
+++ Webseite/Script/onscroll.js
@@ -30,31 +30,84 @@ window.addEventListener('scroll',function()
         hi.style.opacity = '1';  
     }
 
-
-    if(rect.top < window.innerHeight && rect.bottom > 0){
-
-        console.log('im Bildschirm')
-
-        /* Fisch img bewegen*/
-        
-
-          }
-          else {
-            console.log(' nicht im Bildschirm')
-          }
-
-   
-    
-    
-    for (const key in rect) {
-      if (typeof rect[key] !== "function") {
-        console.log(`${key} : ${rect[key]}`)
-        
-      }
+    if(moved == false)
+    move();
+    if(isLarged == false){
+    grow();
     }
+
+
     
 
-});
+})
+
+
+
+
+
+let interval = setInterval(function() {
+    if(isLarged == false){
+    grow();
+    }
+  }, 100);
+
+let isLarged = false;
+let moved = false;
+
+
+function grow(){
+
+    let rectfi = fi.getBoundingClientRect();
+    let rectzi = zi.getBoundingClientRect();
+    let scrolly = window.scrollY;
+
+    if (rectfi.right >= rectzi.left && isLarged == false) {
+        zi.style.visibility = "hidden";
+        fi.src = "Bild/shark.png";
+        
+
+        /* let currentWidth = parseFloat(window.getComputedStyle(fi).width);
+        let currentHeight = parseFloat(window.getComputedStyle(fi).height);
+        let newWidth = currentWidth * 1.2; 
+        let newHeight = currentHeight * 1.2; 
+        fi.style.width = newWidth + "px";
+        fi.style.height = newHeight + "px"; */
+        isLarged = true;
+
+        
+
+      }
+
+
+}
+
+
+
+
+
+function move() {
+
+    let rectfi = fi.getBoundingClientRect();
+    let rectzi = zi.getBoundingClientRect();
+    let scrolly = window.scrollY;
+
+    if (rectfi.top < window.innerHeight && rectfi.bottom > 0) {
+  
+      fi.style.transition = 'transform 4s ease'; 
+        fi.style.transform = `translateX(${rectzi.left - 150}px)`;
+        
+        moved = true;
+
+      
+    } else {
+      console.log('Nicht im Bildschirm');
+        fi.style.transition = 'transform 2s ease'; 
+        fi.style.transform = 'translateX(0)';
+    }
+
+
+    
+  }
 
 
 
